f11850d52801dc62e77038a429ff9094cde6d04d
mt60395 <63754980+mt60395@users.noreply.github.com>
2021-03-09 17:24:21 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+Python script that prints properties of an element based on the provided element
+symbol.
+
+I currently only need this for finding where an element is on the periodic
+table.
---
+++ main.py
@@ -0,0 +1,53 @@
+import json
+import os
+import urllib.request as urllib
+
+
+def main():
+    symbol = format_symbol(input("Element symbol: "))
+    element = find_element_by_symbol(symbol)
+    if element:
+        find = {  # Properties to find and print
+            "Atomic number": "number"  # [To print]:[Property in the .json]
+        }
+        for attribute in find:
+            print(attribute + ": " +
+                  str(get_element_property(element, find[attribute])))
+    else:
+        print("Element does not exist.")
+
+
+def get_dictionary():  # fetches the periodic table dictionary
+    if not os.path.isfile('periodic-table-lookup.json'):
+        urllib.urlretrieve(
+            'https://raw.githubusercontent.com/Bowserinator/Periodic-Table-JSON/master/periodic-table-lookup.json',
+            'periodic-table-lookup.json')
+    f = open('periodic-table-lookup.json')
+    return json.loads(f.read())
+
+
+dictionary = get_dictionary()
+
+
+def format_symbol(symbol):  # formats symbol property. Upper, then lower
+    symbol = symbol.upper()
+    if len(symbol) > 1:
+        return symbol[0] + symbol[1].lower()
+    return symbol
+
+
+def find_element_by_symbol(symbol):
+    for e in dictionary:
+        for a in dictionary[e]:
+            if a == "symbol" and dictionary[e][a] == symbol:
+                return dictionary[e]
+
+
+def get_element_property(element, attribute):
+    for a in element:
+        if a == attribute:
+            return element[a]
+
+
+if __name__ == "__main__":
+    main()
